2efe2ce39ce887451a1a1ea3548e6a2b952b6508
周立 <li.zhou@huilianyi.com>
2019-01-13 16:54:53 +0800

    a

--- src/main/java/com/vole/film/FilmApplication.java
+++ src/main/java/com/vole/film/FilmApplication.java
@@ -2,11 +2,18 @@ package com.vole.film;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FilmApplication {
+public class FilmApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmApplication.class, args);
+	}
+	@Override//为了打包springboot项目
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
